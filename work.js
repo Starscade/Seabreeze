889ba9fc1b705120b935a8372bc86b26093499cc
Starscade <angus@starsca.de>
2026-09-06 16:37:31 -0700

    Reversed offline logic.

--- work.js
+++ work.js
@@ -10,3 +10,19 @@ self.addEventListener('activate', (event) => {
 	)
 	self.clients.claim()
 })
+
+self.addEventListener('fetch', (event) => {
+	if (event.request.method !== 'GET') return
+
+	event.respondWith(
+		fetch(event.request)
+			.then((response) => {
+				const copy = response.clone()
+				caches.open('offline-cache').then((cache) =>
+					cache.put(event.request, copy)
+				)
+				return response
+			})
+			.catch(() => caches.match(event.request)),
+	)
+})
